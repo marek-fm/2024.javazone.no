20726668b559be14018e2e74439314c1fa789694
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-12-12 15:20:32 +0100

    Dont have .vercel in repo
    Also update text in partner

--- .vercel/output/functions/render.func/chunks/generic_b4bd0076.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_8ab974b8.mjs').then(n => n.a);
-
-export { page };
